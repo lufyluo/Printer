3079757cdc7b8d6292e847f19b912aa85b3fb9c8
lufyluo <lufyhome@gmail.com>
2018-04-15 08:59:42 +0800

    1、修改主程序名称

--- PrinterWithChrome/Properties/Resources.Designer.cs
+++ PrinterWithChrome/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PrinterWithChrome.Properties {
+namespace yulaila.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PrinterWithChrome.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PrinterWithChrome.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("yulaila.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
