d7ba0f293acea258b53e7f30fc3a38dde194b159
lufyluo <lufyhome@gmail.com>
2018-04-24 20:05:46 +0800

    提交日志类

---
+++ Printer.Framework/Log/Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer.Framework.Log
+{
+    public class Logger
+    {
+        private static readonly string fileName = "startLog";
+        /// <summary>
+        /// 日志部分
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="content"></param>
+        public static void WriteLogs(string type, string content)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            if (!string.IsNullOrEmpty(path))
+            {
+                path = AppDomain.CurrentDomain.BaseDirectory + fileName;
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                path = path + "\\" + DateTime.Now.ToString("yyyyMMdd");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                path = path + "\\" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+                if (!File.Exists(path))
+                {
+                    FileStream fs = File.Create(path);
+                    fs.Close();
+                }
+                if (File.Exists(path))
+                {
+                    StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default);
+                    sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff  ") + type + "-->" + content);
+                    //  sw.WriteLine("----------------------------------------");
+                    sw.Close();
+                }
+            }
+        }
+
+        public static void Info(string content)
+        {
+            WriteLogs("info", content);
+        }
+        public static void Error(string content)
+        {
+            WriteLogs("error", content);
+        }
+    }
+}
